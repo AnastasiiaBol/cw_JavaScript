5faeee5767e0a6499b7083af68afb4b49ac02e9e
Anastasiia Bolkun <nastiabolkun957@gmail.com>
2025-05-27 19:57:57 +0200

    first JS lesson

---
+++ script.js
@@ -0,0 +1,46 @@
+// let x
+//number
+// x = 10.2345678;  //Byte Tiny Integer Short Integer Long Integer Float
+// //string
+// x = '10.234567';
+// x = "10";
+// x = `10`;
+// //boolean
+// x = true
+// x = false
+// //null
+// x = null;
+// //undefined
+// x = undefined;
+
+
+
+
+let y = 10;
+let str = `3456789.567.78908`;
+let b = true;
+
+console.log(+null)
+console.log(+undefined)
+
+console.log(!!null)
+console.log(!!undefined)
+// console.log(+str) //перетворюємо в число
+// console.log(Number(str))
+// console.log(parseInt(str))
+// console.log(parseFloat(str))
+
+// console.log(!!y) // Bool(ean)
+// console.log(Boolean(y)) // Bool(ean)
+// console.log(!!str) // Bool(ean)
+
+
+// console.log(y.toString()) // String
+// console.log(String(y)) // String
+// console.log(`${y}`) // String "10"
+// console.log(`${b}`) // String "10"
+
+
+console.log(typeof null)
+console.log(typeof str)
+
